79f969a0038d1bf26f7e743f7bf2d910a05cc4f9
Kam Dev <96205664+Kam-Discord-Dev@users.noreply.github.com>
2021-12-15 12:33:21 -0800

    Update index.js

--- index.js
+++ index.js
@@ -6,11 +6,17 @@ const discord = require("discord.js");
 let { bot, client } = new Discord.Client();
 
 const Database = require("@replit/database");
-const db = new Database();
+let db = new Database();
 
-let prefix = require(./config.json)
-let tags = require(./config.json)
-let token = require(./config.json)||process.env.token
+let prefix = require(./config.json);
+let tags = require(./config.json);
+let token = require(./config.json)||process.env.token;
+
+client.on("ready", async () => {
+ console.log(`${client.user.tag} is Running!`);
+ console.log(`Prefix: ${prefix}`);
+ console.log(`Token: ${token}`); /* 🠐 You may remove this for bot security reasons 🠐 */
+})
 
 client.on("message", async (message) => {
  if(message.content.startsWith(prefix+tags) {
@@ -19,7 +25,13 @@ let member = message.author;
 var id;
 id = message.author.username;
 
-}
-})
+let collection = await db.get(`collection_id`);
 
-client.login(process.env.token||config.token||token)
+if(!member.id >= collection) return
+
+/* db.set(`collection_${member.id}`, collection + message.author.id); */
+
+}
+});
+
+client.login(process.env.token||config.token||token);
