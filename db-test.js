b5178b69116e07464de872e7659581891f2b8c49
Kam-Discord-Dev <96205664+Kam-Discord-Dev@users.noreply.github.com>
2021-12-15 12:09:30 -0800

    Create db-test.js

---
+++ db-test.js
@@ -0,0 +1,26 @@
+const Database = require("@replit/database");
+const db = new Database();
+const Discord = require("discord.js")
+let client = new Discord.Client()
+
+async(bot,client,message,args)=>{
+
+let member = message.author;
+var id;
+id = message.author.username;
+
+db.set(`collection_${member.id}`, collection + message.author.id)
+
+await console.log(`Collection DB Running`)
+
+let collection = await db.get(`collection_id`);
+
+if(!member.id >= collection) return
+
+var count
+count = 5
+
+for (var count = 0; count < count; count++) { 
+  console.log(count)
+}
+}
