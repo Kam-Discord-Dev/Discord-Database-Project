4b16ad06252949ee34d20ca2993d223b777eafec
Kam Dev <96205664+Kam-Discord-Dev@users.noreply.github.com>
2021-12-16 11:58:41 -0800

    Create login.js

---
+++ commands/login.js
@@ -0,0 +1,37 @@
+const Discord = require("discord.js");
+let client = new Discord.Client();
+let util = require("djs-simple-utils"); /* My package btw */
+let superEmbed = require("djs-simple-utils");
+const { MessageEmbed, Collection } = require("discord.js");
+const fs = require("fs");
+const axios = require("axios");
+
+const Database = require("@replit/database");
+let db = new Database();
+
+let { tags }  = require("./config.json");
+let { token } = require("./config.json")||process.env.token;
+
+module.exports = {
+  name: "login",
+  aliases: ["l"],
+  description: `Simple login command for ${client.user.tag}`,
+  execute: async (client, message, args) => {
+    
+   let member = message.author;
+   var id;
+   var username;
+   id = message.author.id;
+   id = message.author.username;
+   var oauth = Math.floor(Math.random() * 100000) + 1;
+   let collection = await db.get(`collection_id`);
+    
+    let embed= new Discord.MessageEmbed()
+    .setTitle("User Login")
+    
+   member.send(embed).then(() => {
+     member.send(`This is your OAuth2 Code. Please send it to me when you are told to.`)
+   });
+   
+  }
+}
